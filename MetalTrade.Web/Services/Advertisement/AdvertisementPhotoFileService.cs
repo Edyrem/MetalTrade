04012c4d1662983bbfbe276ed7548f954b9ebffd
Esen <esen.tilekovv99@gmail.com>
2025-11-06 17:51:55 +0600

    Add service AdvertisementPhotoFileService
     #Add file_path, combine file_path and file_name;
     #Add using, in the param. add var. stream, assign obj. of the FileStream, call photo.CopyToAsync. Call saved_paths.Add, indic. file path;
     #Return saved_paths;

---
+++ MetalTrade.Web/Services/Advertisement/AdvertisementPhotoFileService.cs
@@ -0,0 +1,31 @@
+﻿namespace MetalTrade.Web.Services.Advertisement
+{
+    public class AdvertisementPhotoFileService
+    {
+        private readonly IWebHostEnvironment _env;
+        public AdvertisementPhotoFileService(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        public async Task<List<string>> SavePhotos(IFormFile[] photos)
+        {
+            var saved_paths = new List<string>();
+
+            string upload_folder = Path.Combine(_env.WebRootPath, "images", "advertisement");
+            if (!Directory.Exists(upload_folder))
+                Directory.CreateDirectory(upload_folder);
+
+            foreach (var photo in photos)
+            {
+                var file_name = Guid.NewGuid().ToString() + Path.GetExtension(photo.FileName);
+                var file_path = Path.Combine(upload_folder, file_name);
+
+                using (var stream = new FileStream(file_path, FileMode.Create))
+                    await photo.CopyToAsync(stream);
+
+                saved_paths.Add($"/images/advertisement/{file_name}");
+            }
+            return saved_paths;
+        }
+    }
+}
