687b6e967a2b6b1900aeded2ed6ddd8823ccf492
Esen <esen.tilekovv99@gmail.com>
2025-11-06 15:27:42 +0600

    Add service AdvertisementService, add field _repository, init. in the const

---
+++ MetalTrade.Business/Services/AdvertisementService.cs
@@ -0,0 +1,20 @@
+﻿using MetalTrade.DataAccess.Data;
+using MetalTrade.DataAccess.Repositories;
+using MetalTrade.Domain.Entities;
+
+namespace MetalTrade.Business.Services
+{
+    public class AdvertisementService
+    {
+        private readonly AdvertisementRepository _repository;
+        public AdvertisementService(MetalTradeDbContext context)
+        {
+            _repository = new AdvertisementRepository(context);
+        }
+        public async Task Create(Advertisement ads)
+        {
+            await _repository.CreateAsync(ads);
+            await _repository.SaveChangesAsync();
+        }
+    }
+}
