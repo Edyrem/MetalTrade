a8ecb4ce2586bb6c10a641a80015289e7eb3994a
Esen <esen.tilekovv99@gmail.com>
2025-11-06 16:31:08 +0600

    Add AdvertisementController. Add field adsService, init. in the constructor. Add action Create

---
+++ MetalTrade.Web/Controllers/AdvertisementController.cs
@@ -0,0 +1,19 @@
+﻿using MetalTrade.Business.Services;
+using MetalTrade.DataAccess.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetalTrade.Web.Controllers
+{
+    public class AdvertisementController : Controller
+    {
+        AdvertisementService adsService;
+        public AdvertisementController(MetalTradeDbContext context)
+        {
+            adsService = new AdvertisementService(context);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
