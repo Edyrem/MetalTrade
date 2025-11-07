e7e123deec8054ccd3afecf2a4f1a07741441365
Esen <esen.tilekovv99@gmail.com>
2025-11-07 15:50:58 +0600

    Delete files IAdvertisementPhotoRepository and AdvertisementPhotoRepository

--- MetalTrade.DataAccess/Interfaces/Repositories/IAdvertisementPhotoRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using MetalTrade.Domain.Entities;
-
-namespace MetalTrade.DataAccess.Interfaces.Repositories
-{
-    public interface IAdvertisementPhotoRepository: IRepository<AdvertisementPhoto>
-    {
-    }
-}
