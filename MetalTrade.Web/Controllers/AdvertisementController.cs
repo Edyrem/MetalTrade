4a137761d08ae35fce3fc829d71f6032eebf3489
Esen <esen.tilekovv99@gmail.com>
2025-11-07 15:59:23 +0600

    Remove function of saving ads photo in the controller

--- MetalTrade.Web/Controllers/AdvertisementController.cs
+++ MetalTrade.Web/Controllers/AdvertisementController.cs
@@ -1,7 +1,6 @@
 ﻿using MetalTrade.Business.Services;
 using MetalTrade.DataAccess.Data;
 using MetalTrade.Domain.Entities;
-using MetalTrade.Web.Services.Advertisement;
 using MetalTrade.Web.ViewModels.Advertisement;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -11,15 +10,11 @@ namespace MetalTrade.Web.Controllers
     public class AdvertisementController : Controller
     {
         private readonly AdvertisementService adsService;
-        private readonly AdvertisementPhotoService adsPhotoService;
-        private readonly AdvertisementPhotoSaveService adsPhotoSaveService;
         private readonly UserManager<User> _userManager;
 
-        public AdvertisementController(MetalTradeDbContext context, IWebHostEnvironment env, UserManager<User> userManager)
+        public AdvertisementController(MetalTradeDbContext context, UserManager<User> userManager)
         {
             adsService = new AdvertisementService(context);
-            adsPhotoService = new AdvertisementPhotoService(context);
-            adsPhotoSaveService = new AdvertisementPhotoSaveService(env);
             _userManager = userManager;
         }
         public IActionResult Create()
@@ -40,29 +35,10 @@ namespace MetalTrade.Web.Controllers
                     PhoneNumber = model.PhoneNumber ?? string.Empty,
                     City = model.City
                 };
-                if (false)
-                {
-                    User? user = await _userManager.GetUserAsync(User);
-                    if (user != null)
-                        ads.UserId = user.Id;
-                    int adsId = await adsService.CreateAsync(ads);
-                    if (model.Images != null)
-                    {
-                        List<string> photoLinks = await adsPhotoSaveService.SavePhotosAsync(model.Images);
-                        if (photoLinks.Count > 0)
-                        {
-                            foreach (var link in photoLinks)
-                            {
-                                var adsPhoto = new AdvertisementPhoto()
-                                {
-                                    PhotoLink = link,
-                                    AdvertisementId = adsId
-                                };
-                                await adsPhotoService.CreateAsync(adsPhoto);
-                            }
-                        }
-                    }
-                }
+                User? user = await _userManager.GetUserAsync(User);
+                if (user != null)
+                    ads.UserId = user.Id;
+                int adsId = await adsService.CreateAsync(ads);
                 return Json(ads);
             }
             return View(model);
