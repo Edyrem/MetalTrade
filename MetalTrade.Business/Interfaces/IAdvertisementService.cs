eb0d54627d5bd88dc346f35ce20dbe9ebea63d04
Esen <esen.tilekovv99@gmail.com>
2025-11-07 18:30:21 +0600

    Add IAdvertisementService

---
+++ MetalTrade.Business/Interfaces/IAdvertisementService.cs
@@ -0,0 +1,15 @@
+﻿using MetalTrade.Domain.Entities;
+
+namespace MetalTrade.Business.Interfaces
+{
+    internal interface IAdvertisementService
+    {
+        Task GetAsync(int advertisementId);
+        Task GetAllAsync();
+        Task CreateAsync();
+        Task UpdateAsync();
+        Task DeleteAsync();
+        Task CreatePhotosAsync();
+        Task DeletePhotoAsync();
+    }
+}
