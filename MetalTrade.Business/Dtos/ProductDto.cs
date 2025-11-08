98e99906d6e64259b57f22b408f5fd626c96a077
Esen <esen.tilekovv99@gmail.com>
2025-11-08 17:01:22 +0600

    Add ProductDto, clone prop. from entity Product

---
+++ MetalTrade.Business/Dtos/ProductDto.cs
@@ -0,0 +1,13 @@
+﻿using MetalTrade.Domain.Entities;
+
+namespace MetalTrade.Business.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int MetalTypeId { get; set; }
+        public MetalType? MetalType { get; set; }
+        public List<AdvertisementDto>? Advertisements { get; set; }
+    }
+}
