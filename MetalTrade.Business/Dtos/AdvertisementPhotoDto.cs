5bcd77dbddb70da31d072c6fd609bef54295d4a8
Esen <esen.tilekovv99@gmail.com>
2025-11-08 16:31:23 +0600

    Add dto AdvertisementPhotoDto, clone prop. from AdvertisementPhoto

---
+++ MetalTrade.Business/Dtos/AdvertisementPhotoDto.cs
@@ -0,0 +1,17 @@
+﻿using MetalTrade.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetalTrade.Business.Dtos
+{
+    internal class AdvertisementPhotoDto
+    {
+        public int Id { get; set; }
+        public string PhotoLink { get; set; }
+        public int AdvertisementId { get; set; }
+        public Advertisement? Advertisement { get; set; }
+    }
+}
