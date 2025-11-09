8c6759ac03bd99940258c46c5d749bffdcee3dda
Esen <esen.tilekovv99@gmail.com>
2025-11-09 17:03:28 +0600

    Add actions Delete(get, post). Add view model DeleteViewModel. Add view Delete

---
+++ MetalTrade.Web/ViewModels/Advertisement/DeleteViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MetalTrade.Web.ViewModels.Advertisement
+{
+    public class DeleteViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+    }
+}
