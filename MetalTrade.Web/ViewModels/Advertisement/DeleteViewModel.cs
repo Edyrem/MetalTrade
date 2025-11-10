d37eb201bc97cc46871338b67c452ec97dd2c015
Esen <esen.tilekovv99@gmail.com>
2025-11-10 14:00:28 +0600

    Add attribute Display to the prop. Title in the DeleteViewModel(Advertisement)

--- MetalTrade.Web/ViewModels/Advertisement/DeleteViewModel.cs
+++ MetalTrade.Web/ViewModels/Advertisement/DeleteViewModel.cs
@@ -1,8 +1,11 @@
-﻿namespace MetalTrade.Web.ViewModels.Advertisement
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MetalTrade.Web.ViewModels.Advertisement
 {
     public class DeleteViewModel
     {
         public int Id { get; set; }
+        [Display(Name = "Название")]
         public string Title { get; set; } = string.Empty;
     }
 }
