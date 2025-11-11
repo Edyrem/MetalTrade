94078a4783a8e528437b4f1c71a02a97c893a684
Gazimjanov Egor <Egar.net2000@gmail.com>
2025-11-12 04:57:29 +0600

    Del IMapWith, add MappingProfile

---
+++ MetalTrade.Business/Common/Mapping/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MetalTrade.Business.Dtos;
+using MetalTrade.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetalTrade.Business.Common.Mapping
+{
+    internal class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Advertisement, AdvertisementDto>().ReverseMap();
+            CreateMap<AdvertisementPhoto, AdvertisementPhotoDto>().ReverseMap();
+            CreateMap<MetalType, MetalTypeDto>().ReverseMap();
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<User, UserDto>().ReverseMap();
+        }
+    } 
+}
