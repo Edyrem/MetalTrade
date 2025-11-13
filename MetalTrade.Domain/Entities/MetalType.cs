e823fd7426239817615f46c1e9dd66a571b46bee
Taiyrbek Anarbekov <an.taku2455@gmail.com>
2025-11-13 22:52:47 +0600

    4-8

--- MetalTrade.Domain/Entities/MetalType.cs
+++ MetalTrade.Domain/Entities/MetalType.cs
@@ -1,11 +1,8 @@
-using MetalTrade.DataAccess.Interfaces.Repositories;
-
 namespace MetalTrade.Domain.Entities;
 
-public class MetalType  : ISoftDeletable
+public class MetalType
 {
     public int Id { get; set; }
     public string Name { get; set; }
     private List<Product> Products { get; set; }
-    public bool IsDeleted { get; set; }
 }
--- MetalTrade.Domain/Entities/Product.cs
+++ MetalTrade.Domain/Entities/Product.cs
@@ -1,13 +1,10 @@
-using MetalTrade.DataAccess.Interfaces.Repositories;
-
 namespace MetalTrade.Domain.Entities;
 
-public class Product : ISoftDeletable
+public class Product
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public int MetalTypeId { get; set; }
     public MetalType? MetalType { get; set; }
     public List<Advertisement> Advertisements { get; set; }
-    public bool IsDeleted { get; set; }
 }
