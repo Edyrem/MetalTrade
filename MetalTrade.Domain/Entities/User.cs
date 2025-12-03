adee6555f9471a7b60ce466b52a8f83f7549df30
Taiyrbek Anarbekov <an.taku2455@gmail.com>
2025-12-04 05:37:08 +0600

    MD-47

--- MetalTrade.Domain/Entities/User.cs
+++ MetalTrade.Domain/Entities/User.cs
@@ -6,8 +6,7 @@ namespace MetalTrade.Domain.Entities
     public class User: IdentityUser<int>, ISoftDeletable
     {
         public string Photo { get; set; }
-        //временно, еще не решили
-        public string? WhatsAppNumber { get; set; }
+        public string WhatsAppNumber { get; set; }
         List<Advertisement> Advertisements { get; set; }
         public bool IsDeleted { get; set; }
     }
