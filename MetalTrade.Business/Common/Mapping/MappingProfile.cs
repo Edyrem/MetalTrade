330af50b9a1814130d6a5064e82fc72fcee63ad9
Edyrem <edyrem88@gmail.com>
2025-12-09 23:29:54 +0600

    Исправил редактирование объявлений

--- MetalTrade.Business/Common/Mapping/MappingProfile.cs
+++ MetalTrade.Business/Common/Mapping/MappingProfile.cs
@@ -10,16 +10,19 @@ namespace MetalTrade.Business.Common.Mapping
         {
             CreateMap<Advertisement, AdvertisementDto>()
                 .ForMember(dest => dest.PhotoFiles, opt => opt.Ignore())
-                .ForMember(dest => dest.CreateDate, opt => opt.Ignore())
-                .ForMember(dest => dest.Product, opt => opt.Ignore())
-                .ForMember(dest => dest.User, opt => opt.Ignore())
+                //.ForMember(dest => dest.CreateDate, opt => opt.Ignore())
+                //.ForMember(dest => dest.Product, opt => opt.Ignore())
+                //.ForMember(dest => dest.User, opt => opt.Ignore())
                 .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber ?? ""))
                 .ForMember(dest => dest.Body, opt => opt.MapFrom(src => src.Body ?? ""))
-                .ForMember(dest => dest.Photoes, opt => opt.Ignore())
-                .ForMember(dest => dest.Status, opt => opt.Ignore())
-                .ForMember(dest => dest.IsAd, opt => opt.Ignore())
-                .ForMember(dest => dest.IsTop, opt => opt.Ignore())                
-                .ReverseMap();
+                //.ForMember(dest => dest.Photoes, opt => opt.Ignore())
+                //.ForMember(dest => dest.Status, opt => opt.Ignore())
+                //.ForMember(dest => dest.IsAd, opt => opt.Ignore())
+                //.ForMember(dest => dest.IsTop, opt => opt.Ignore())                
+                .ReverseMap()
+                .ForMember(d => d.Id, opt => opt.Ignore())
+                .ForMember(d => d.ProductId, opt => opt.Ignore())
+                .ForMember(d => d.Product, opt => opt.Ignore());
 
             CreateMap<User, UserDto>()
                 .ForMember(dest => dest.Photo, opt => opt.Ignore()) 
@@ -29,14 +32,17 @@ namespace MetalTrade.Business.Common.Mapping
             CreateMap<AdvertisementPhotoDto, AdvertisementPhoto>().ReverseMap();
 
             #region MetalService
-            CreateMap<MetalTypeDto, MetalType>()
+            CreateMap<MetalType, MetalTypeDto>()
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(dest => dest.Name.ToLower()))
-                .ForMember(dest => dest.Products, opt => opt.Ignore());
+                .ForMember(dest => dest.Products, opt => opt.Ignore())
+                .ForMember(dest => dest.Products, opt => opt.Ignore())
+                .ReverseMap();
             #endregion
 
             #region ProductService
             CreateMap<ProductDto, Product>()
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(dest => dest.Name.ToLower()))
+                .ForMember(dest => dest.Advertisements, opt => opt.Ignore())
                 .ReverseMap();
             #endregion
 
