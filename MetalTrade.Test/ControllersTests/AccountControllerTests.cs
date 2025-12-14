9fd8cdbecfc00dec6c8c8d82a88640123911467f
taku-24-55 <secret.player2455@gmail.com>
2025-12-14 22:10:32 +0600

    md-61-final

---
+++ MetalTrade.Test/ControllersTests/AccountControllerTests.cs
@@ -0,0 +1,157 @@
+using MetalTrade.Test.Helpers;
+using MetalTrade.Web.ViewModels;
+using MetalTrade.Web.ViewModel;
+using MetalTrade.Business.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using System.Security.Claims;
+using Moq;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace MetalTrade.Test.ControllersTests;
+
+public class AccountControllerTests : ControllerTestBase
+{
+    public AccountControllerTests()
+    {
+        InitAccountController();
+    }
+    
+
+    private void SetUnauthenticated()
+    {
+        AccountController.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity())
+            }
+        };
+    }
+
+    private void SetAuthenticated()
+    {
+        var identity = new ClaimsIdentity(
+            new[] { new Claim(ClaimTypes.Name, "user123") },
+            "mock"
+        );
+
+        AccountController.ControllerContext.HttpContext.User =
+            new ClaimsPrincipal(identity);
+    }
+
+    private RegisterViewModel ValidRegisterModel() =>
+        new()
+        {
+            UserName = "User1",
+            Email = "user@gmail.com",
+            PhoneNumber = "500500500",
+            WhatsAppNumber = "500500500",
+            Password = "Qweqwe1!",
+            PasswordConfirm = "Qweqwe1!"
+        };
+    
+
+    [Fact]
+    public void RegisterGetReturnsView()
+    {
+        var result = AccountController.Register();
+        Assert.IsType<ViewResult>(result);
+    }
+
+    [Fact]
+    public void RegisterGetAuthenticatedRedirects()
+    {
+        SetAuthenticated();
+
+        var result = AccountController.Register();
+
+        var redirect = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Index", redirect.ActionName);
+        Assert.Equal("Advertisement", redirect.ControllerName);
+    }
+
+    [Fact]
+    public async Task RegisterPostInvalidModelReturnsView()
+    {
+        AccountController.ModelState.AddModelError("Email", "Required");
+
+        var model = new RegisterViewModel();
+        var result = await AccountController.Register(model);
+
+        var view = Assert.IsType<ViewResult>(result);
+        Assert.Equal(model, view.Model);
+
+        UserMock.Verify(
+            s => s.CreateUserAsync(It.IsAny<UserDto>(), "user"),
+            Times.Never
+        );
+    }
+
+    [Fact]
+    public async Task RegisterPostCreateFailedReturnsView()
+    {
+        UserMock
+            .Setup(s => s.CreateUserAsync(It.IsAny<UserDto>(), "user"))
+            .ReturnsAsync(false);
+
+        var result = await AccountController.Register(ValidRegisterModel());
+
+        Assert.IsType<ViewResult>(result);
+        Assert.False(AccountController.ModelState.IsValid);
+    }
+
+    [Fact]
+    public async Task RegisterPostSuccessLoginRedirects()
+    {
+        UserMock
+            .Setup(s => s.CreateUserAsync(It.IsAny<UserDto>(), "user"))
+            .ReturnsAsync(true);
+
+        UserMock
+            .Setup(s => s.LoginAsync(It.IsAny<string>(), It.IsAny<string>(), false))
+            .ReturnsAsync(SignInResult.Success);
+
+        var result = await AccountController.Register(ValidRegisterModel());
+
+        var redirect = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Index", redirect.ActionName);
+        Assert.Equal("Advertisement", redirect.ControllerName);
+    }
+    
+
+    [Fact]
+    public async Task LoginPostSuccessRedirects()
+    {
+        UserMock
+            .Setup(s => s.LoginAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>()))
+            .ReturnsAsync(SignInResult.Success);
+
+        var model = new LoginViewModel
+        {
+            Login = "user",
+            Password = "pass",
+            RememberMe = false
+        };
+
+        var result = await AccountController.Login(model);
+
+        var redirect = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Index", redirect.ActionName);
+        Assert.Equal("Advertisement", redirect.ControllerName);
+    }
+
+    [Fact]
+    public async Task LogoutAuthenticatedCallsServiceAndRedirects()
+    {
+        SetAuthenticated();
+
+        var result = await AccountController.Logout();
+
+        UserMock.Verify(s => s.LogoutAsync(), Times.Once);
+
+        var redirect = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Login", redirect.ActionName);
+    }
+}
--- MetalTrade.Test/Helpers/ControllerTestBase.cs
+++ MetalTrade.Test/Helpers/ControllerTestBase.cs
@@ -1,6 +1,9 @@
-﻿using AutoMapper;
+﻿using System.Security.Claims;
+using AutoMapper;
 using MetalTrade.Business.Interfaces;
 using MetalTrade.Web.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 
 namespace MetalTrade.Test.Helpers
@@ -10,11 +13,43 @@ namespace MetalTrade.Test.Helpers
         protected Mock<IProductService> ProductMock = new();        
         protected Mock<IMetalService> MetalMock = new();
         protected Mock<IMapper> MapperMock = new();
+        protected Mock<IImageUploadService> ImageUploadMock = new();
+        protected Mock<IUserService> UserMock = new();
 
         protected ProductController ProductController
             => new(ProductMock.Object, MetalMock.Object, MapperMock.Object);
 
         protected MetalTypeController MetalTypeController
             => new(MetalMock.Object, MapperMock.Object);
+        
+        protected AccountController AccountController { get; private set; }
+
+        protected void InitAccountController()
+        {
+            AccountController = new AccountController(
+                UserMock.Object,
+                MapperMock.Object,
+                ImageUploadMock.Object
+            );
+
+            AccountController.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity())
+                }
+            };
+        }
+
+        protected void SetAuthenticated()
+        {
+            var identity = new ClaimsIdentity(
+                new[] { new Claim(ClaimTypes.Name, "user1111") },
+                "mock"
+            );
+
+            AccountController.ControllerContext.HttpContext.User =
+                new ClaimsPrincipal(identity);
+        }
     }
 }
