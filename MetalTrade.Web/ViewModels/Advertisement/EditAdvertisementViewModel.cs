7f2df448ef2c6b0d288f58928c7ecbbdcd0f688d
Almaz Amanov <almazbekow@gmail.com>
2026-01-06 23:11:12 +0600

    Изменены валидации поля PhoneNumber во вьюмоделях

--- MetalTrade.Web/ViewModels/Advertisement/EditAdvertisementViewModel.cs
+++ MetalTrade.Web/ViewModels/Advertisement/EditAdvertisementViewModel.cs
@@ -6,29 +6,39 @@ namespace MetalTrade.Web.ViewModels.Advertisement
     public class EditAdvertisementViewModel
     {
         public int Id { get; set; }
+        
         [Display(Name = "Название")]
-        [Required(ErrorMessage = "Поле Название объязательно")]
+        [Required(ErrorMessage = "Поле Название обязательно")]
         [StringLength(200, ErrorMessage = "Можно вводить не более 200 символов")]
         public string Title { get; set; } = string.Empty;
+        
         [Display(Name = "Описание")]
         [StringLength(2000, ErrorMessage = "Можно вводить не более 2000 символов")]
-        [Required(ErrorMessage = "Поле Описание объязательно")]
+        [Required(ErrorMessage = "Поле Описание обязательно")]
         public string Body { get; set; } = string.Empty;
+        
         [Display(Name = "Цена")]
-        [Required(ErrorMessage = "Поле Цена объязательно")]
+        [Required(ErrorMessage = "Поле Цена обязательно")]
         public decimal Price { get; set; }
+        
         [Display(Name = "Адрес")]
         [StringLength(2000, ErrorMessage = "Можно вводить не более 2000 символов")]
         public string? Address { get; set; }
+        
         [Display(Name = "Номер телефона")]
-        [StringLength(100, ErrorMessage = "Можно вводить не более 100 символов")]
-        [Required(ErrorMessage = "Поле Номер телефона объязательно")]
+        [MaxLength(9, ErrorMessage = "Максимальное количество цифр- 9")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})$",
+            ErrorMessage = "Некорректный ввод номера телефона. Введите 9 цифр. <br/> Пример : (555) (555) (555)")]
+        [Required(ErrorMessage = "Поле Номер телефона обязательно")]
         public string PhoneNumber { get; set; } = string.Empty;
+        
         [Display(Name = "Город")]
         [StringLength(200, ErrorMessage = "Можно вводить не более 200 символов")]
         public string? City { get; set; }
+        
         [Display(Name = "Продукт")]
-        [Required(ErrorMessage = "Поле Продукт объязательно")]
+        [Required(ErrorMessage = "Поле Продукт обязательно")]
         public int ProductId { get; set; }
         public List<ProductViewModel> Products { get; set; } = [];
         public int UserId { get; set; }
