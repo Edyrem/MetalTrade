63d482188fa659aef0a99951c1bf807ade9190ef
Almaz Amanov <almazbekow@gmail.com>
2026-01-09 19:17:36 +0600

    Изменен AdvertisementImportDto, добавлена валидация на поля

--- MetalTrade.Business/Dtos/AdvertisementImportDto.cs
+++ MetalTrade.Business/Dtos/AdvertisementImportDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,32 @@ namespace MetalTrade.Business.Dtos
 {
     public class AdvertisementImportDto
     {
-        public string Title { get; set; }
+        [Required] 
+        [StringLength(200, ErrorMessage = "Можно вводить не более 200 символов")]
+        public string Title { get; set; } = string.Empty;
+        
+        [Required]
+        [StringLength(2000, ErrorMessage = "Можно вводить не более 2000 символов")]
+        public string Body { get; set; } = string.Empty;
+        
+        [Required]
+        [Range(1, 1000000, ErrorMessage = "Цена не может быть отрицательной или быть равной нулю!")]
         public decimal Price { get; set; }
-        public string Unit { get; set; }
-        public string Description { get; set; }
-        public string City { get; set; }
-        public string Contacts { get; set; }
-        public int? MetalTypeId { get; set; }
-        public int? ProductId { get; set; }
+        
+        [Required]
+        [MaxLength(9)]
+        [RegularExpression(@"^[0-9]{9}$")]
+        public string PhoneNumber { get; set; } = string.Empty;
+        
+        [Required]
+        public string ProductName { get; set; }= string.Empty;
+        
+        [StringLength(200, ErrorMessage = "Можно вводить не более 200 символов")]
+        public string? City { get; set; }
+        
+        [StringLength(2000, ErrorMessage = "Можно вводить не более 2000 символов")]
+        public string? Address { get; set; }
+        
+        //public string Unit { get; set; }
     }
 }
