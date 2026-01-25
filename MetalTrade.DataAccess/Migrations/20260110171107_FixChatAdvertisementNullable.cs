6f8ee45ea62f1fe97a25a77aa21085c1d4e945d2
taku-24-55 <secret.player2455@gmail.com>
2026-01-25 14:49:07 +0600

    миграции

---
+++ MetalTrade.DataAccess/Migrations/20260110171107_FixChatAdvertisementNullable.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MetalTrade.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixChatAdvertisementNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Chats_Advertisements_AdvertisementId",
+                table: "Chats");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AdvertisementId",
+                table: "Chats",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Chats_Advertisements_AdvertisementId",
+                table: "Chats",
+                column: "AdvertisementId",
+                principalTable: "Advertisements",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Chats_Advertisements_AdvertisementId",
+                table: "Chats");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AdvertisementId",
+                table: "Chats",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Chats_Advertisements_AdvertisementId",
+                table: "Chats",
+                column: "AdvertisementId",
+                principalTable: "Advertisements",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
