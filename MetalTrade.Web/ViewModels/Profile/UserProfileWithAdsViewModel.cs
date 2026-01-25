6c7ee6db86cd89de5f047829c19c1b947060cbfc
taku-24-55 <secret.player2455@gmail.com>
2026-01-25 14:49:17 +0600

    вм

--- MetalTrade.Web/ViewModels/Profile/UserProfileWithAdsViewModel.cs
+++ MetalTrade.Web/ViewModels/Profile/UserProfileWithAdsViewModel.cs
@@ -1,20 +1,22 @@
-using MetalTrade.Business.Dtos;
+    using MetalTrade.Business.Dtos;
 
-namespace MetalTrade.Web.ViewModels.Profile
-{
-    public class UserProfileWithAdsViewModel
+    namespace MetalTrade.Web.ViewModels.Profile
     {
-        public string UserName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
-        public string WhatsAppNumber { get; set; }
-        public string? PhotoPath { get; set; }
+        public class UserProfileWithAdsViewModel
+        {
+            public string UserName { get; set; } = string.Empty;
+            public string Email { get; set; } = string.Empty;
+            public string PhoneNumber { get; set; } = string.Empty;
+            public string WhatsAppNumber { get; set; }
+            public string? PhotoPath { get; set; }
 
-        public bool IsSupplier { get; set; }
+            public bool IsSupplier { get; set; }
 
-        public List<AdvertisementDto>? Advertisements { get; set; }
+            public List<AdvertisementDto>? Advertisements { get; set; }
 
-        /*public List<AdvertisementDto>? Favorites { get; set; }*/
+            public int UserId { get; set; }
 
+            /*public List<AdvertisementDto>? Favorites { get; set; }*/
+
+        }
     }
-}
